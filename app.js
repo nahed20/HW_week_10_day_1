853afb4f18b9b5200d60a3f15e6b60b3d4caae80
Nahed_2019 <n.add55@hotmail.com>
2019-03-18 03:40:51 +0300

    first commit

--- app.js
+++ app.js
@@ -39,6 +39,62 @@ app.post('/api/v1/todos', (req, res) => {
         message: 'todo added successfully',
         todo
     })
+
+    app.get('/api/v1/todos/:id', (req, res) => {
+        const id = parseInt(req.params.id, 10);
+        db.map((todo) => {
+            if (todo.id === id) {
+                return res.status(200).send({
+                    success: 'true',
+                    message: 'todo retrieved successfully',
+                    todo,
+                });
+            }
+        });
+        return res.status(404).send({
+            success: 'false',
+            message: 'todo does not exist',
+        });
+    }); app.get('/api/v1/todos/:id', (req, res) => {
+        const id = parseInt(req.params.id, 10);
+        db.map((todo) => {
+            if (todo.id === id) {
+                return res.status(200).send({
+                    success: 'true',
+                    message: 'todo retrieved successfully',
+                    todo,
+                });
+            }
+        });
+        return res.status(404).send({
+            success: 'false',
+            message: 'todo does not exist',
+        });
+    });
+    app.delete('/api/v1/todos/:id', (req, res) => {
+        const id = parseInt(req.params.id, 10);
+
+        db.map((todo, index) => {
+            if (todo.id === id) {
+                db.splice(index, 1);
+                return res.status(200).send({
+                    success: 'true',
+                    message: 'Todo deleted successfuly',
+                });
+            }
+        });
+
+
+        return res.status(404).send({
+            success: 'false',
+            message: 'todo not found',
+        });
+
+
+    });
+
+
+
 });
 
 
--- db/db.js
+++ db/db.js
@@ -3,6 +3,15 @@ const todos = [
         id: 1,
         title: "lunch",
         description: "Go for lunc by 2pm"
+    },
+    {
+        name: "Ola",
+        school: "unilag"
+    },
+
+    {
+        title: "breakfast",
+        description: "get breakfast"
     }
 ];
 
